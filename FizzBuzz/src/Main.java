c5bf5627538a929421c8c13c521c1137f105d69b
blaiseitt <blazej.poniatowski@gmail.com>
2022-05-24 19:17:35 +0200

    FizzBuzz status: done.

---
+++ FizzBuzz/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.fizzBuzz(10));
+    }
+
+
+}
---
+++ FizzBuzz/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<Integer> listOfIntegers = createListOfIntegers(n);
+        List<String> out = new ArrayList<>();
+        for(int i : listOfIntegers){
+            if(i%3==0 && i%5==0){
+                out.add("FizzBuzz");
+            } else if (i%3==0) {
+                out.add("Fizz");
+            }else if(i%5==0){
+                out.add("Buzz");
+            }else{
+                out.add(Integer.toString(i));
+            }
+        }
+        return out;
+    }
+
+    private static List<Integer> createListOfIntegers(int n){
+        List<Integer>list = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            list.add(i);
+        }
+        return list;
+    }
+}
