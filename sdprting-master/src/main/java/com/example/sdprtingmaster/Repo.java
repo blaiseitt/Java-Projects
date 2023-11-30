3f50b201cfeffa13a2ef668d65f4aad5a2f899bb
blaiseitt <blazej.poniatowski@gmail.com>
2023-11-30 02:03:04 +0100

    es

--- sdprting-master/src/main/java/com/example/sdprtingmaster/Repo.java
+++
@@ -1,6 +0,0 @@
-package com.example.sdprtingmaster;
-
-public interface Repo {
-
-    String chuj();
-}
--- sdprting-master/src/main/java/com/example/sdprtingmaster/RepoImplEnhanced.java
+++
@@ -1,4 +0,0 @@
-package com.example.sdprtingmaster;
-
-public class RepoImplEnhanced extends RepoImpl {
-}
